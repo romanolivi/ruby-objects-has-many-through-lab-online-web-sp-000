6165ba7a0bc316d7e66ca937e94da8e7512922b4
Romano Livi <romano_livi@outlook.com>
2020-12-22 18:50:44 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment| 
+      appointment.patient == self
+    end
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+end
