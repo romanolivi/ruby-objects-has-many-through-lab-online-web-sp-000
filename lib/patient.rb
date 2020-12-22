986af7d6f1aa0ddba265bae713b337df3908f2ad
Romano Livi <romano_livi@outlook.com>
2020-12-22 18:34:35 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment| 
+      appointment.patient == self
+    end
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+end
